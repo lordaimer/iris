dfa77cee1ab8d84b106670df5b8f4c5f403a7b38
lordaimer <araneaimer@gmail.com>
2025-12-29 14:08:12 +0530

    chore[chocolatey]: initial Chocolatey package files

---
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Remove the iris.exe file
+$exePath = Join-Path $toolsDir "iris.exe"
+if (Test-Path $exePath) {
+    Remove-Item $exePath -Force
+    Write-Host "Removed iris.exe from $toolsDir"
+}
