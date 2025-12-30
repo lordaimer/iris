6249312625914db61e624bd60d605a185ce7a07a
lordaimer <araneaimer@gmail.com>
2025-12-30 14:31:04 +0530

    fix[CI]: point url64bit to a static link for every release instead of the dynamic /latest endpoint
    need to update the chocolatey github actions workflow to automatically update the version variable in chocolateyinstall.ps1 as well as the release notes url in iris.nuspec. also need to add a new valid icon file (iris.png) at asssets/icon for the packaging to work properly

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version = '1.3.5'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url64bit       = 'https://github.com/lordaimer/iris/releases/latest/download/iris-windows-amd64.exe'
+  url64bit       = "https://github.com/lordaimer/iris/releases/download/v$version/iris-windows-amd64.exe"
   checksum64     = 'be61dd6a47c92dcda027d123f467bcf42e1eb32efd63aa5def7e0522aec61aef'
   checksumType64 = 'sha256'
 }
